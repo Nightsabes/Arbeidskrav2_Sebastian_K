36fcf89e8857f4ecc00ca84a8d29930c2b430885
Nightsabes <39102045+Nightsabes@users.noreply.github.com>
2026-02-10 14:38:44 +0100

    Did something works on tomorrow

--- handlekurv/src/App.jsx
+++ handlekurv/src/App.jsx
@@ -1,10 +1,22 @@
+import AddForm from './components/AddForm'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const shopItems = [
+    {
+      id: 0,
+      title: "Melk",
+      content: "anal"
+    },
+    {
+      id: 1,
+      title: "Egg",
+      content: "regret"
+    }
+  ]
+  const [count, setCount] = useState()
 
   return (
     <>
