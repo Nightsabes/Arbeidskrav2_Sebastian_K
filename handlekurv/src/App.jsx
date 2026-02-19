3bfc2d112593cdbf1c2ec6ec81f02837dda48c23
Nightsabes <39102045+Nightsabes@users.noreply.github.com>
2026-02-19 05:29:21 +0100

    ActualProgress

--- handlekurv/src/App.jsx
+++ handlekurv/src/App.jsx
@@ -16,19 +16,43 @@ function App() {
       let formData = new FormData(form)
       let formDataObj = Object.fromEntries(formData.entries())
       formDataObj.purchased = false;
-      setShoppingList(formDataObj)
+      setShoppingList([...shoppingList,formDataObj])
   }
   
+  const removeShoppingItem = (event)=>{
+    let removeItem = event.target.value;
+    let newShopList = shoppingList.filter(function(shoppingItem){
+        if(removeItem === shoppingItem.shoppingItemName){
+          return false;
+        }
+        else{
+          return true;
+        }
+    })
+    setShoppingList(newShopList)
+
+  }
 
   return (
     <>
       <h1>Handlelist</h1>
-      <div>
+      <div className='card'>
         <form onSubmit={addShoppingItem}>
           <input type="text" name='shoppingItemName' placeholder='Egg..'/>
           <button>Legg til vare</button>
         </form>
       </div>
+      {
+        shoppingList.map(function(val,index){
+          return(
+          <div className='card' key={index}>
+            <span><input type="checkbox" /></span>
+            <span>{val.shoppingItemName}</span>
+            <span><button onClick={removeShoppingItem} value={val.shoppingItemName}>Remove</button></span>
+          </div>
+          )
+        })
+      }
     </>
   )
 }
